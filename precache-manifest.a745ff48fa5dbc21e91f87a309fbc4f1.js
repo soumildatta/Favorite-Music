d0fffa8bedc3c44afa9d11d9004529a16cdc72ac
Soumil Datta <soumildatta@gmail.com>
2020-06-29 22:00:23 -0500

    Updates

---
+++ precache-manifest.a745ff48fa5dbc21e91f87a309fbc4f1.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12bb53edf7f36a3392279b6b973d7db7",
+    "url": "/Favorite-Music/index.html"
+  },
+  {
+    "revision": "01fba087b719a2c126f7",
+    "url": "/Favorite-Music/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "217a53f07cb33fb98362",
+    "url": "/Favorite-Music/static/css/main.95df7ef7.chunk.css"
+  },
+  {
+    "revision": "01fba087b719a2c126f7",
+    "url": "/Favorite-Music/static/js/2.9752680e.chunk.js"
+  },
+  {
+    "revision": "ad951958ed7766763ed3e6c72917393c",
+    "url": "/Favorite-Music/static/js/2.9752680e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "217a53f07cb33fb98362",
+    "url": "/Favorite-Music/static/js/main.0c4cf63f.chunk.js"
+  },
+  {
+    "revision": "9b15c06b9f85d3a58457",
+    "url": "/Favorite-Music/static/js/runtime-main.300f66bd.js"
+  },
+  {
+    "revision": "cd6a511277fd190a84bca6fdc41d5ee1",
+    "url": "/Favorite-Music/static/media/EmptyHeart.cd6a5112.svg"
+  },
+  {
+    "revision": "0bcfec201bec76f2691ee14c7af03fce",
+    "url": "/Favorite-Music/static/media/background.0bcfec20.jpg"
+  },
+  {
+    "revision": "32d5e34bbf3e563234704be0d78530bc",
+    "url": "/Favorite-Music/static/media/heart.32d5e34b.svg"
+  },
+  {
+    "revision": "f9719b880bef0431e7f8da7ed04ed98a",
+    "url": "/Favorite-Music/static/media/search.f9719b88.svg"
+  }
+]);
