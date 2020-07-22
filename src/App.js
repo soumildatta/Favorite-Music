850a16b3fa8e4ea59534ad5dd5f3796ccac053a6
Soumil Datta <soumildatta@gmail.com>
2020-07-21 21:21:30 -0500

    Update description

--- src/App.js
+++ src/App.js
@@ -59,10 +59,10 @@ class App extends Component {
         <div className="App-header">
           <h1>Soumil's Favorite Songs</h1>
           <p>
-            This is a list of my current favorite songs. In case youre curious
-            if I like a certain song, use the search bar below. Leave a like on
-            songs to let me know you like them too! You can also see the total
-            number of people that have liked the song :)
+            This is a list of my current favorite songs. You can search through
+            my favorite songs using the search bar below. Leave a like on songs
+            to let me know you like them too! You can also see the total number
+            of people that have liked the song :)
           </p>
 
           <div className="song-cards">
