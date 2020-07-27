1529fe39f3a52b4b3fbe30beeddc9e0ef70bbe31
Soumil Datta <soumildatta@gmail.com>
2020-07-27 13:12:16 -0500

    Change search bar to fit the theme

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -6,17 +6,19 @@ import SearchIcon from "./img/search.svg";
 class Search extends Component {
   render() {
     return (
-      <form className="form-inline d-flex justify-content-center md-form form-sm pb-5">
-        <div className="searchbar">
-          <input
-            onChange={(e) => this.props.search(e)}
-            className="search_input"
-            type="text"
-            name=""
-            placeholder="Search song or artist"
-          />
-          <div className="search_icon">
-            <img src={SearchIcon} alt="Search icon"></img>
+      <form className="form-inline d-flex justify-content-center md-form form-sm pb-2">
+        <div class="p-1 bg-light rounded rounded-pill shadow-sm mb-4">
+          <div class="input-group">
+            <input
+              onChange={(e) => this.props.search(e)}
+              type="search"
+              placeholder="Search artist or song..."
+              aria-describedby="button-addon1"
+              class="form-control border-0 bg-light ml-1"
+            />
+            <div class="input-group-append mx-2 search-icon">
+              <img src={SearchIcon} alt="Search icon"></img>
+            </div>
           </div>
         </div>
       </form>
